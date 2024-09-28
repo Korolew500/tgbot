3462fda856631eeae64aca5614f8505c1d95f5b9
user <korolew500@gmail.com>
2024-09-28 21:47:21 +0800

    commit for template.env

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ from loader import bot
 import telebot
 import handlers
 from utils.set_bot_commands import set_default_commands
+# import sqlite3
+# from peewee import SqliteDatabase, Model, CharField, IntegerField
 
 if __name__ == "__main__":
-    set_default_commands(bot)
-    bot.infinity_polling()
+   set_default_commands(bot)
+   bot.infinity_polling()
