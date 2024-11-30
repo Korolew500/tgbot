e033d1c283e67b6d1542931a585d3616734685d0
user <korolew500@gmail.com>
2024-11-30 20:54:33 +0800

    fixed metals

--- handlers/default_handlers/metals.py
+++ handlers/default_handlers/metals.py
@@ -28,9 +28,10 @@ def metals_answer(callback_query):
 
 def metals_logic() -> str:
     # Дата в формате 02/03/2002
-    # date_req1 = (datetime.date.today() - datetime.timedelta(days=1)).strftime("%d/%m/%Y")
-    date_req1 = datetime.date.today().strftime("%d/%m/%Y")
-    url = f"https://www.cbr.ru/scripts/xml_metall.asp?date_req1={date_req1}&date_req2={date_req1}"
+    # Берется отрезок в несколько дней из-за бага при отсутсвии данных в выходные дни
+    date_req1 = (datetime.date.today() - datetime.timedelta(days=10)).strftime("%d/%m/%Y")
+    date_req2 = datetime.date.today().strftime("%d/%m/%Y")
+    url = f"https://www.cbr.ru/scripts/xml_metall.asp?date_req1={date_req1}&date_req2={date_req2}"
     response = requests.get(url)
 
     if response.status_code == 200:
@@ -38,7 +39,7 @@ def metals_logic() -> str:
         root = El.fromstring(response.content)
         val_text = ''
 
-        for metall in root.findall('Record'):
+        for metall in root.findall('Record')[-4:]:
             code = metall.get('Code')
             code = ['золота', 'серебра', 'платины', 'палладия'][int(code)-1]
             value = metall.find('Buy').text
