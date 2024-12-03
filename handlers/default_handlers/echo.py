6c100a759032c8103e80bf4b061e65cb4d1a27fb
Korolev Aleksandr <112260289+Korolew500@users.noreply.github.com>
2024-12-03 22:08:00 +0800

    Update echo.py

--- handlers/default_handlers/echo.py
+++ handlers/default_handlers/echo.py
@@ -1,7 +1,11 @@
 from telebot.types import Message
 from loader import bot
+from handlers.default_handlers.inline import gen_markup, delite_inline
 
 
 @bot.message_handler(state=None)
 def bot_echo(message: Message):
-    bot.reply_to(message, message.text)
+    bot.reply_to(message,
+                 f'Ваша команда <{message.text}> не понятна, возможно опечатка. \n'
+                 f'Попробуйте ещё раз 😊',
+                 reply_markup = gen_markup())
